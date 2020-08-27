7152b20bb310428e9320ccab823bc6cc59da603b
Amedeus Akira DSouza <59585791+aadsouza@users.noreply.github.com>
2020-08-27 15:25:02 -0700

    uses shortname; postype instead of recruiter_type

--- jobmarket.py
+++ jobmarket.py
@@ -23,7 +23,8 @@ inst_data = pd.DataFrame(db_cursor.fetchall())
 inst_data["startdate"] = pd.to_datetime(inst_data["startdate"]) #convert object to datetime
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 listfoo = [{"label": "From Institutions - All", "value": 0}]
-listextendfoo = [{"label": i, "value": j} for i, j in sorted(set(zip(inst_data.from_institution_name, inst_data.from_oid)))]
+listextendfoo = [{"label": i, "value": j} for i, j in sorted(set(zip(inst_data.from_shortname, inst_data.from_oid)))] #inst_data.from_institution_name
+#FIXME ideally instead of shortname we create new column inst_data_from_fullname = inst_data.from_institution_name + " - " + inst_data.from_department also + "(" + inst_data.from_shortname + ")" either via MySQL or Python conditional on server; same for to
 fooextender = listfoo.extend(listextendfoo)
 
 vals = []
@@ -39,42 +40,60 @@ app.layout = html.Div([
                        #html.Div("Either the From Institution or the Primary Fields must be set to something other than All for this to work"),
                        dcc.Dropdown(id = "select_inst",
                                     options = listfoo,
-                                    value = 67,
+                                    value = 67, #UBC
                                     multi = True,
                                     placeholder = "Select institution where they graduated from"
                                     ),
                        dcc.Dropdown(id = "select_stuff",
-                                    options = [{"label": "Primary Specializations - All", "value": 0}, 
-                                               {"label": "Development; Growth", "value": 1},
-                                               {"label": "Econometrics", "value": 2},
-                                               {"label": "Finance", "value": 6},
-                                               {"label": "Industrial Organization", "value": 7},
-                                               {"label": "Labor; Demographic Economics", "value": 10},
-                                               {"label": "Macroeconomics; Monetary", "value": 12},
-                                               {"label": "Microeconomics", "value": 13},
-                                               {"label": "Theory", "value": 15},
-                                               {"label": "Behavioral Economics", "value": 16},
-                                               {"label": "Political Economics", "value": 23}],
-                                    value = 0,
+                                    options = [{"label": "Primary Specializations - All", "value": "0"}, 
+                                               {"label": "Development; Growth", "value": "1"},
+                                               {"label": "Econometrics", "value": "2"},
+                                               {"label": "Finance", "value": "6"},
+                                               {"label": "Industrial Organization", "value": "7"},
+                                               {"label": "Labor; Demographic Economics", "value": "10"},
+                                               {"label": "Macroeconomics; Monetary", "value": "12"},
+                                               {"label": "Microeconomics", "value": "13"},
+                                               {"label": "Theory", "value": "15"},
+                                               {"label": "Behavioral Economics", "value": "16"},
+                                               {"label": "Political Economics", "value": "23"}],
+                                    value = "0",
                                     placeholder = "Select primary specialization"
                                     ),
                        dcc.Dropdown(id = "select_sector",
-                                    options = [{"label": "Recruiter Types - All", "value": "0"},
-                                               {"label": "Academic organization (economics department)", "value": "1"},
-                                               {"label": "Academic organization (business school)", "value": "2"},
-                                               {"label": "Academic organization (agricultural/resource economics department)", "value": "3"},
-                                               {"label": "Academic organization (other than econ, business, or ag econ)", "value": "4"},
-                                               {"label": "Government agency or commission", "value": "5"},
-                                               {"label": "Private (for profit) business or organization", "value" : "6"},
-                                               {"label": "Private (non-profit) business or organization", "value": "7"},
-                                               {"label": "Other type of organization", "value": "8"},
-                                               {"label": "Advertising agency or executive recruiter", "value": "9"},
-                                               {"label": "Human Resources department of educational or non-profit institution", "value": "10"},
-                                               {"label": "Human Resources department of for-profit organization", "value": "11"},
-                                               {"label": "Personal", "value": "12"}],
+                                    options = [{"label": "Position Type - All", "value": "0"}, 
+                                               {"label": "Assistant Professor", "value": "1"},
+                                               {"label": "Associate Professor", "value": "2"},
+                                               {"label": "Full Professor", "value": "3"},
+                                               {"label": "Professor Any Level", "value": "4"},
+                                               {"label": "Temporary Lecturer", "value": "5"},
+                                               {"label": "Post-Doc", "value": "6"},
+                                               {"label": "Lecturer", "value": "7"},
+                                               {"label": "Consultant", "value": "8"},
+                                               {"label": "Other Academic", "value": "9"},
+                                               {"label": "Other Non-Academic", "value": "10"},
+                                               {"label": "Tenured Professor", "value": "11"},
+                                               {"label": "Assistant or Associate Professor", "value": "13"},
+                                               {"label": "Visiting Professor/Lecturer/Instructor", "value": "15"}],
                                     value = "0",
-                                    placeholder = "Select a type of recruiter"
-                                    ),
+                                    placeholder = "Select position type"
+                                    ),    
+#                        dcc.Dropdown(id = "select_sector",
+#                                     options = [{"label": "Recruiter Types - All", "value": "0"},
+#                                                {"label": "Academic organization (economics department)", "value": "1"},
+#                                                {"label": "Academic organization (business school)", "value": "2"},
+#                                                {"label": "Academic organization (agricultural/resource economics department)", "value": "3"},
+#                                                {"label": "Academic organization (other than econ, business, or ag econ)", "value": "4"},
+#                                                {"label": "Government agency or commission", "value": "5"},
+#                                                {"label": "Private (for profit) business or organization", "value" : "6"},
+#                                                {"label": "Private (non-profit) business or organization", "value": "7"},
+#                                                {"label": "Other type of organization", "value": "8"},
+#                                                {"label": "Advertising agency or executive recruiter", "value": "9"},
+#                                                {"label": "Human Resources department of educational or non-profit institution", "value": "10"},
+#                                                {"label": "Human Resources department of for-profit organization", "value": "11"},
+#                                                {"label": "Personal", "value": "12"}],
+#                                     value = "0",
+#                                     placeholder = "Select a type of recruiter"
+#                                     ),
                        dcc.Dropdown(id = "slidey", 
                                     options = vals,
                                     value = 2019,
@@ -103,7 +122,7 @@ def mapinator(q, x, y, z):
     iterated_data = inst_data.loc[((inst_data["startdate"].dt.year == int(z)) | ((inst_data["startdate"].dt.year > int(z)*int(z)))) &\
                                   ((inst_data["from_oid"].isin(q)) | ((inst_data["from_oid"] > max(q)*max(q)*max(q)))) &\
                                   ((inst_data["category_id"] == int(x)) | ((inst_data["category_id"] > int(x)*400))) &\
-                                  ((inst_data["recruiter_type"] == int(y)) | ((inst_data["recruiter_type"] > int(y)*40)))]
+                                  ((inst_data["postype"] == int(y)) | ((inst_data["postype"] > int(y)*40)))]
 #pairing "all" with specific institutions gives specific institutions
                                    
     fig = go.Figure()
@@ -115,14 +134,14 @@ def mapinator(q, x, y, z):
 #        else: colors = "blue"
         colors = "navy"
         fig.add_trace(go.Scattergeo(lon = [row.longitude, row.to_longitude], lat = [row.latitude, row.to_latitude], mode = "lines", line = dict(width = 1, color = colors)))
-        fig.add_trace(go.Scattergeo(lon = [row.to_longitude], lat = [row.to_latitude], hoverinfo = "text", text = [row.to_name + '; Rank: ' + str(row.to_rank)], mode = "markers", marker = dict(size = 2.5, color = "navy", line = dict(width = 3, color = "darkgoldenrod"))))
-        fig.add_trace(go.Scattergeo(lon = [row.longitude], lat = [row.latitude], hoverinfo = "text", text = [row.from_institution_name + '; Rank: ' + str(row.rank)], mode = "markers", marker = dict(size = 2.5, color = "navy", line = dict(width = 3, color = "darkgoldenrod"))))
-        cloud_data.append(dict({'from_institution_name':row.from_institution_name,'rank':row.rank,'to_name':row.to_name,'to_rank':row.to_rank})) #'aid':row.aid
+        fig.add_trace(go.Scattergeo(lon = [row.to_longitude], lat = [row.to_latitude], hoverinfo = "text", text = [row.to_shortname + '; Rank: ' + str(row.to_rank)], mode = "markers", marker = dict(size = 2.5, color = "navy", line = dict(width = 3, color = "darkgoldenrod")))) #row.to_name
+        fig.add_trace(go.Scattergeo(lon = [row.longitude], lat = [row.latitude], hoverinfo = "text", text = [row.from_shortname + '; Rank: ' + str(row.rank)], mode = "markers", marker = dict(size = 2.5, color = "navy", line = dict(width = 3, color = "darkgoldenrod")))) #row.from_institution_name
+        cloud_data.append(dict({'from_shortname':row.from_shortname,'rank':row.rank,'to_shortname':row.to_shortname,'to_rank':row.to_rank, 'position_name': row.position_name})) #'aid':row.aid #'from_institution_name':row.from_institution_name
 
     fig.update_layout(showlegend = False, geo = dict(projection_type = "equirectangular", showland = True, landcolor = "whitesmoke", countrycolor = "silver", showcountries = True, showlakes = True, showcoastlines = True, coastlinecolor = "darkgrey", lakecolor = "white", oceancolor = "white")) #title_text = "category_id_" + str(x) + ": " + data_subsets[x].name.unique()[0], 
 
 
-    cloud_columns = [{"name":"graduated-from","id":"from_institution_name"},{"name":"gradschool-rank","id":"rank"},{"name":"hired-by","id":"to_name"},{"name":"hired-by-rank","id":"to_rank"}] #{"name":"aid","id":"aid"},
+    cloud_columns = [{"name":"graduated-from","id":"from_shortname"},{"name":"gradschool-rank","id":"rank"},{"name":"hired-by","id":"to_shortname"},{"name":"hired-by-rank","id":"to_rank"}, {"name":"position-type","id":"position_name"}] #{"name":"aid","id":"aid"},
     return fig, cloud_data, cloud_columns
     
 
