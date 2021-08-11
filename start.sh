7bbe81ea0997c83781550db2feb7b71ea06a36dd
mike <peters@econ.ubc.ca>
2021-08-11 15:03:09 -0700

    added logging to start script

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-gunicorn -w 4 -b 0.0.0.0:8025 -D jobmarket:server
+gunicorn -w 4 -b 0.0.0.0:8025 -D jobmarket:server --access-logfile /home/peters/dash_logs/access_log --error-logfile /home/peters/dash_logs/error_log 
